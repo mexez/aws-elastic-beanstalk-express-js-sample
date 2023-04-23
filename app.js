d30a44ecd19f0fab556a8a1612d71eba33eb4ce8
mexez <eezeali@gmail.com>
2023-04-23 12:10:56 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('eoan checks'));
+app.get('/', (req, res) => res.send('Apr23_review'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
